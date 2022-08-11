2d12f0c0b0b3e52148612e7d648b4b8bc78a267c
Nabilla <nabillaomar88@gmail.com>
2022-08-11 22:05:02 +0800

    Initial commit - 11-08-2022

---
+++ app/src/main/java/com/nabilla/retrofittutorial/Todo.kt
@@ -0,0 +1,8 @@
+package com.nabilla.retrofittutorial
+
+data class Todo(
+    val completed: Boolean,
+    val id: Int,
+    val title: String,
+    val userId: Int
+)
---
+++ app/src/main/java/com/nabilla/retrofittutorial/TodoApi.kt
@@ -0,0 +1,12 @@
+package com.nabilla.retrofittutorial
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface  TodoApi {
+
+    @GET("/todos")
+    fun getTodos(): Response<List<Todo>>
+
+}
